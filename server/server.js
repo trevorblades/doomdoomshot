3b78920293e221f6f9134bbca92bfe6bb9271574
Trevor Blades <tdblades@gmail.com>
2018-04-30 23:10:12 -0700

    Reorganize server stuff

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
+const io = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const websocket = socketio(server);
 server.listen(process.env.PORT, () =>
   console.log(`listening on port ${process.env.PORT}`)
 );
 
+const websocket = io(server);
 websocket.on('connection', socket =>
   console.log(`a client just joined on ${socket.id}`)
 );
