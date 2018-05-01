9c30104ab278407848095c3e3e7fc9e274e9e654
Trevor Blades <tdblades@gmail.com>
2018-04-30 23:39:52 -0700

    Use local server in development

--- app/socket.js
+++ app/socket.js
@@ -1,3 +1,6 @@
+/* global __DEV__ */
 import io from 'socket.io-client';
 
-export default io('http://localhost:3000');
+export default io(
+  __DEV__ ? 'http://localhost:3000' : 'https://server.doomdoomshot.com'
+);
