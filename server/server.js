9ca0eda5b15b99c22e4e3bcb7d65dee404689687
Trevor Blades <tdblades@gmail.com>
2018-05-05 17:00:11 -0400

    Move game setup to its own func

--- server/server.js
+++ server/server.js
@@ -18,51 +18,53 @@ const hsetAsync = promisify(client.hset).bind(client);
 const hmsetAsync = promisify(client.hmset).bind(client);
 const hgetallAsync = promisify(client.hgetall).bind(client);
 
-function getGameHandler(id, socket) {
-  return async message => {
-    await hsetAsync(id, socket.id, message);
-    const game = await hgetallAsync(id);
+function setupGame(game, socket) {
+  socket.send(game);
+  socket.on('message', async message => {
+    await hsetAsync(game.id, socket.id, message);
+    const nextGame = await hgetallAsync(game.id);
     socket
-      .send(game)
+      .send(nextGame)
       .to(game.player1)
       .to(game.player2)
-      .send(game);
-  };
+      .send(nextGame);
+  });
 }
 
+const QUEUE_KEY = 'queue';
 const websocket = io(server);
 websocket.on('connection', async socket => {
   // Grab a random opponent and remove them from the queue
-  const opponent = await spopAsync('queue');
+  const opponent = await spopAsync(QUEUE_KEY);
   if (!opponent) {
     // If there aren't any opponents, add the current user to the queue
-    await saddAsync('queue', socket.id);
+    await saddAsync(QUEUE_KEY, socket.id);
 
     const subscriber = client.duplicate();
     subscriber.subscribe(socket.id);
     subscriber.on('message', async (channel, gameId) => {
+      subscriber.quit();
+
       const game = await hgetallAsync(gameId);
-      socket.send(game);
-      socket.on('message', getGameHandler(gameId, socket));
+      setupGame(game, socket);
     });
 
     socket.on('disconnect', () => {
       subscriber.quit();
-      client.srem('queue', socket.id);
+      client.srem(QUEUE_KEY, socket.id);
     });
     return;
   }
 
-  const gameId = shortid.generate();
   const game = {
+    id: shortid.generate(),
     player1: opponent,
     player2: socket.id,
     [socket.id]: '🙅',
     [opponent]: '🙅‍',
   };
 
-  await hmsetAsync(gameId, game);
-  client.publish(opponent, gameId);
-  socket.send(game);
-  socket.on('message', getGameHandler(gameId, socket));
+  await hmsetAsync(game.id, game);
+  client.publish(opponent, game.id);
+  setupGame(game, socket);
 });
